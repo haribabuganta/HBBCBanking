6b7558f3767f848fdd30a5f12f985d65f9d2ec3f
hari <haricg8@gmail.com>
2019-05-14 10:54:20 +0530

    initial setup

---
+++ src/main/java/com/hbbc/hbbcbank/HbbcbankApplication.java
@@ -0,0 +1,13 @@
+package com.hbbc.hbbcbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbbcbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbbcbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hbbc/hbbcbank/HbbcbankApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hbbc.hbbcbank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HbbcbankApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
